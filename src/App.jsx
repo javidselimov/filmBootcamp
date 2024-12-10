3beb8c0c5a209df4d7e6e48791d0c0fc67f8e18e
Javid  Z Salimov <44189780+javidselimov@users.noreply.github.com>
2024-12-10 17:41:53 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import { Route } from 'react-router-dom';
+import MainPage from './pages/MainPage/MainPage';
+import ListPage from './pages/ListPage/ListPage';
+
+import './reset.css';
+import './common.css';
+
+const App =()=> {
+  
+    return (
+      <div className="app">
+        <Route path="/" exact component={MainPage} />
+        <Route path="/list/:id" exact component={ListPage} />
+      </div>
+    );
+  
+}
+
+export default App;
