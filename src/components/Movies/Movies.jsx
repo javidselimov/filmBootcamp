3220cb4f1727953546a7683fc69313b96763fd29
Memmedova Peri <perimemmedova20052022@gmail>
2024-12-16 02:59:04 +0400

    Movies done.

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react';
 import MovieItem from '../MovieItem/MovieItem';
 import './Movies.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Movies = () => {
-	const [movies, setMovies] = useState([
-		{
-			imdbID: 'tt3896198',
-			title: 'Guardians of the Galaxy Vol. 2',
-			year: 2017,
-			poster:
-				'https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg',
-		},
-		{
-			imdbID: 'tt0068646',
-			title: 'The Godfather',
-			year: 1972,
-			poster:
-				'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
-		},
-	]);
-
+	const { movies, loading, error } = useSelector((state) => state.movies);
+	// console.log(movies);
 	return (
-		<ul className="movies">
-			{movies?.map((movie) => (
-				<li className="movies__item" key={movie.imdbID}>
-					<MovieItem {...movie} />
-				</li>
-			))}
-		</ul>
+		<div className="movies-div-container">
+			<ul className="movies">
+				{Array.isArray(movies) && movies.length > 0 ? (
+					movies?.map((movie) => (
+						<li className="movies__item" key={movie.imdbID}>
+							<MovieItem {...movie} />
+						</li>
+					))
+				) : (
+					<p>No movies found.</p>
+				)}
+			</ul>
+		</div>
 	);
 };
 
